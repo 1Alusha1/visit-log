07ffdb7b769a3ddf3c8366748f0df9a369cfc55d
1alusha1 <alusha9636@gmail.com>
2023-12-09 15:45:09 +0200

    made route which calculate attendance stats

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -28,6 +28,59 @@ export const createGroup = async (req, res) => {
   await new allOfGroupsModel({
     name,
   }).save();
-  
+
   res.status(200).json({ message: "Группа создна" });
 };
+
+export const chekStatsStudent = async (req, res) => {
+  const { studentName } = req.body;
+
+  const subjects = ["Literature", "EN_lang", "UA_lang", "Math"];
+
+  const student = await studentsModel.findOne({ name: studentName });
+
+  if (!student.attendance.length) {
+    return res.status(404).json({ message: "Студент не посещал занятий" });
+  }
+
+  let stats = {};
+  function rec(subjects) {
+    if (!subjects.length) {
+      return stats;
+    }
+    const count = subjects.length;
+    let arr = [];
+    for (let k = 0; k < student?.attendance.length; k++) {
+      if (student.attendance[k].subject == subjects[count - 1]) {
+        arr.push(student.attendance[k]);
+      }
+    }
+    stats[subjects[count - 1]] = arr;
+    arr = [];
+    subjects.pop();
+    rec(subjects);
+  }
+  rec(subjects);
+  calculate(stats);
+  return res.status(200).json(stats);
+};
+
+function calculate(obj) {
+  for (let elem in obj) {
+    let countWhereWas = 0;
+    const countOfLeason = obj[elem].length;
+
+    for (let i = 0; i < obj[elem].length; i++) {
+      if (obj[elem][i].was) {
+        countWhereWas++;
+      }
+    }
+
+    obj[elem] = {
+      ...obj[elem],
+      countOfLeason,
+      countWhereWas,
+      visitProcent: Math.floor((countWhereWas * 100) / countOfLeason) + "%",
+    };
+  }
+}
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { controleVisit, createGroup } from "../controllers/user.controller.js";
+import {
+  chekStatsStudent,
+  controleVisit,
+  createGroup,
+} from "../controllers/user.controller.js";
 import roleMiddleware from "../middleware/role.middleware.js";
 const router = Router();
 
 router.post("/controle-visit", controleVisit);
 router.post("/create-group", roleMiddleware(["MASTER"]), createGroup);
+router.post("/student-stats", chekStatsStudent);
 export default router;
